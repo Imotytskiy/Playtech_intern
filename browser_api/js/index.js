e137127dbf6029ae44243d1a3aaa6ba132dc4138
Imotytskiy <Imotytskiy@gmail.com>
2023-10-03 19:32:04 +0200

    add change text color

--- browser_api/js/index.js
+++ browser_api/js/index.js
@@ -1,2 +1,12 @@
 // Write your code here
-console.log('Hello world!');
+console.log("Hello world!");
+
+// 1# Change color text
+const elementClass_t1 = document.querySelector(".t1");
+const elementButtonClass_b1 = document.querySelector(".b1");
+
+function changeColor() {
+  elementClass_t1.style.color = "blue";
+}
+
+elementButtonClass_b1.addEventListener("click", changeColor);
