1ca56ca56f7498cb07a729f55c88d68b37396c95
Imotytskiy <Imotytskiy@gmail.com>
2023-10-03 20:43:50 +0200

    add change background color

--- browser_api/js/index.js
+++ browser_api/js/index.js
@@ -1,12 +1,23 @@
 // Write your code here
 console.log("Hello world!");
 
-// 1# Change color text
+// 1# Change text color
 const elementClass_t1 = document.querySelector(".t1");
 const elementButtonClass_b1 = document.querySelector(".b1");
 
-function changeColor() {
+function changeTextColor() {
   elementClass_t1.style.color = "blue";
 }
 
-elementButtonClass_b1.addEventListener("click", changeColor);
+elementButtonClass_b1.addEventListener("click", changeTextColor);
+
+// 2# Change background color
+
+const elementClass_t2 = document.querySelector(".t2");
+const elementButtonClass_b2 = document.querySelector(".b2");
+
+function changeBackgroundColor() {
+  elementClass_t2.style.backgroundColor = "blue";
+}
+
+elementButtonClass_b2.addEventListener("click", changeBackgroundColor);
